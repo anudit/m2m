f52d1fc7953f391f82970254865229e616f14d1b
Anudit Nagar <nagaranudit@gmail.com>
2019-01-01 18:18:26 +0530

    File Name Sanitization

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import os
 import urllib
+import re
 
 def parseArticle(soup):
     samples = soup.find_all("div", "section-inner")
-    tags = samples[0].find_all(['h1','h2','h3','h4','ol','blockquote','figure','p'])
+    tags = samples[0].find_all(['h1','h2','h3','h4','ol','ul','blockquote','figure','p'])
 
     doc = ""
 
     for i in tags:
-        #print(i.name)
+        print(i.name)
         if (i.name == "h1"):
             doc+=("# " + i.getText() + "\n")
         elif (i.name == "h2"):
@@ -66,22 +67,29 @@ if (r.status_code != 200):
 
 parsed_uri = urlparse(LINK)
 if (parsed_uri.netloc != "medium.com"):
-    print("Not a Medium Link")
-    exit()
+    ans = input("Not a Medium Link, Continue? (Y/N) ")
+    if (ans.lower() != "y"):
+        exit()
+
+print("Parsing Article")
 
 result = requests.get(LINK)
 soup = BeautifulSoup(result.content, features="html.parser")
-FILENAME = "./Articles/" + soup.title.string + ".md"
+fn  = soup.title.string
+forb = '<>:/\|?*'
+fn = ''.join(c for c in fn if c not in forb)
+FILENAME = "./Articles/" + fn[0:252] + ".md"
+
 
 if(os.path.exists("Articles") != True):
     os.mkdir("Articles")
+
 
 if (os.path.exists(FILENAME) == True):
     ans = input("Article already exists, download again? (Y/N) ")
     if (ans.lower() == "y"):
         try:
             with open(FILENAME, "w", encoding="utf8") as file:     
-                print("Parsing Article")
                 file.write(parseArticle(soup))
                 print("Done, Happy Reading!")  
         except IOError:
